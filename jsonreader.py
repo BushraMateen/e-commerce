3d7acb16945c281e493d5b58309943192cef392e
mastanpatel <mastanpatel1993@gmail.com>
2021-10-05 12:11:01 +0530

    created a list of products from json file object

--- jsonreader.py
+++ jsonreader.py
@@ -1,7 +1,7 @@
 # Python program to read
 # json file
 
-
+from product import product
 import json
 def loadfile():
 # Opening JSON file
@@ -10,14 +10,34 @@ def loadfile():
     # returns JSON object as
     # a dictionary
     data = json.load(f)
+    f.close()
+    return data
+
+
+
+def get_product_list():
+    productlist=[]
+    data=loadfile()
+    for i in data:
+        #print(i["product_name"])
+        p1 = product(i["uniq_id"],i["product_name"],i["retail_price"],i["discounted_price"],i["image"],
+        i["description"],i["product_rating"],i["overall_rating"],i["brand"])
+        productlist.append(p1)
+    return productlist
+
+
 
     # Iterating through the json
     # list
     #print(data[2])
-    for i in data[2]:
-        print(data[2][i])
-        
+    # for i in data[2]:
+    #     print(data[2][i])
+    
+
+    
+        # for j in i:
+        #     print(i)
     #print(data[1])
     # Closing file
-    f.close()
+  
 
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 
 import json
-from jsonreader import loadfile 
-
-loadfile()
-
-
-
-
+from jsonreader import get_product_list, loadfile 
 
 
 # load the json file and save in object 
+#loadfile()
+get_product_list()
+
+
+
+
+
+
+
 
 
 #create list of products from json file object
